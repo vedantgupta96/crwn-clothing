b66c4c3ceeeaa87c7bfb6dd4a5a714854e402df4
vedant gupta <vedantgupta96@gmail.com>
2019-12-03 02:33:48 +0530

    Colection-item and preview code on 3-dec-2019

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import { Route } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component";
+
+const HatsPage = () => <div>HATS PAGE</div>;
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route path="/shop" component={ShopPage}></Route>
+      </switch>
     </div>
   );
 }
